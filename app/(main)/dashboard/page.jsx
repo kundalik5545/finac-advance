ea93b02c512060cbbd474ede69834081768e86ec
kunda <kunda@ADMIN>
2025-08-12 19:30:16 +0530

    udpated - made client component

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,28 @@
-export const dynamic = "force-dynamic"; // add this at the top of your file
-import { getUser } from "@/action/user";
+// export const dynamic = "force-dynamic"; // add this at the top of your file
 
-export default async function DashboardPage() {
-  const user = await getUser();
+// import { getUser } from "@/action/user";
+
+// export default async function DashboardPage() {
+//   const user = await getUser();
+
+//   return (
+//     <div>
+//       <h1>Dashboard Page</h1>
+//       <pre>{JSON.stringify(user, null, 2)}</pre>
+//     </div>
+//   );
+// }
+
+"use client";
+import { getUser } from "@/action/user";
+import { useEffect, useState } from "react";
+
+export default function DashboardPage() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUser().then(setUser);
+  }, []);
 
   return (
     <div>
