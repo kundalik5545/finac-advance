2bb7d7954e317fada119b48d938195a898ad03a4
kunda <kunda@ADMIN>
2025-08-12 18:44:04 +0530

    fix - issue with user is present inside homepage as it should be a client component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
-import { getUser } from "@/action/user";
-
-export default async function Home() {
-  const user = await getUser();
-
+export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Home Page</h1>
